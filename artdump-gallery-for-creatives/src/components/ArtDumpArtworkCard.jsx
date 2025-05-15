02ffa7088279e7f29f1456505aac264e9d0762be
Jian Lee Ramos <139615457+JaeYangLee@users.noreply.github.com>
2025-05-15 19:07:53 +0800

    Fixed ArtWorkCard animations

--- artdump-gallery-for-creatives/src/components/ArtDumpArtworkCard.jsx
+++ artdump-gallery-for-creatives/src/components/ArtDumpArtworkCard.jsx
@@ -10,22 +10,25 @@ function ArtDumpArtworkCard(
   return (
     <>
       {/* Artwork Card Container */}
-      <div className="m-2 w-[430.46px] h-[379.8px] bg-[#F8FAFC] text-[#372F3D] transition-all duration-300 ease-in-out hover:text-[#F8FAFC] shadow-xl/20">
-        {/* Image Thumbnail */}
-        <img
-          src="src/assets/shikamaruImageSample.jpg"
-          alt="Artwork Thumbnail"
-          className="absolute object-cover w-[432px] h-[300px] transition-all duration-300 ease-in-out hover:h-[379.8px] hover:z-0"
-        />
-        {/* Artwork Basic Details */}
-        <div className="p-[315px] flex flex-row justify-between px-4 pb-0">
-          <p className="font-barlow text-[14px] font-bold z-50">
-            Project Title
-          </p>
-          <p className="font-barlow text-[14px] font-bold z-50">Status</p>
+      <div className="m-2 w-[430.46px] h-[379.8px] bg-[#F8FAFC] text-[#372F3D] transition-all duration-300 ease-in-out hover:text-[#F8FAFC] shadow-xl/20 relative overflow-hidden group">
+        {/* Image Thumbnail with Gradient */}
+        <div className="relative w-full h-[300px] transition-all duration-300 ease-in-out">
+          <img
+            src="src/assets/shikamaruImageSample.jpg"
+            alt="Artwork Thumbnail"
+            className="object-cover w-full h-full transition-all duration-300 ease-in-out group-hover:h-[379.8px] -z-10"
+          />
+          {/* Gradient Overlay */}
         </div>
-        <div className="p-1 flex flex-row justify-between px-4 pb-0 z-50">
-          <p className="font-barlow text-[14px] font-light italic z-50">
+        <div className="absolute inset-0 bg-gradient-to-t from-black/50 to-transparent opacity-0  ease-in-out hover:opacity-100 transition-opacity duration-300 z-10"></div>
+
+        {/* Artwork Basic Details */}
+        <div className="px-4 pt-2 pb-0 flex flex-row justify-between z-20 relative">
+          <p className="font-barlow text-[14px] font-bold">Project Title</p>
+          <p className="font-barlow text-[14px] font-bold">Status</p>
+        </div>
+        <div className="px-4 pt-1 pb-0 flex flex-row justify-between z-20 relative">
+          <p className="font-barlow text-[14px] font-light italic">
             Published on: 00/00/0000
           </p>
           <svg
@@ -34,7 +37,7 @@ function ArtDumpArtworkCard(
             height="14"
             viewBox="0 0 18 18"
             fill="none"
-            className="mr-3 z-50"
+            className="mr-3"
           >
             <circle cx="9" cy="9" r="9" fill="#60CB62" />
           </svg>
