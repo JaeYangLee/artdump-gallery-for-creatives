fcb80b350485f47598bd9e2cfdb48b6b0b47be2c
Jian Lee Ramos <139615457+JaeYangLee@users.noreply.github.com>
2025-05-23 02:05:32 +0800

    Built ArtDumpsterAddArtworkPage.=

--- artdump-gallery-for-creatives/src/App.jsx
+++ artdump-gallery-for-creatives/src/App.jsx
@@ -1,11 +1,12 @@
 import "./index.css";
+import ArtDumpsterAddArtworkPage from "./pages/ArtDumpsterAddArtworkPage";
 import ArtDumpsterHeroSectionPage from "./pages/ArtDumpsterHeroSectionPage";
 import ArtDumpsterMyDumpsterPage from "./pages/ArtDumpsterMyDumpsterPage";
 
 function App() {
   return (
     <>
-      <ArtDumpsterMyDumpsterPage />
+      <ArtDumpsterAddArtworkPage />
     </>
   );
 }
