80481daf0f0480847882582b5d4a734d6491304e
Jian Lee Ramos <139615457+JaeYangLee@users.noreply.github.com>
2025-05-23 03:27:59 +0800

    installed router dom

--- artdump-gallery-for-creatives/src/App.jsx
+++ artdump-gallery-for-creatives/src/App.jsx
@@ -1,15 +1,19 @@
+import "./index.css";
 import CancelValidatorModal from "./components/CancelValidatorModal";
 import UploadImageSuccessModal from "./components/UploadImageSuccessModal";
-import "./index.css";
 import ArtDumpsterAddArtworkPage from "./pages/ArtDumpsterAddArtworkPage";
 import ArtDumpsterHeroSectionPage from "./pages/ArtDumpsterHeroSectionPage";
 import ArtDumpsterMyDumpsterPage from "./pages/ArtDumpsterMyDumpsterPage";
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {path:"/", element:}
+]);
 
 function App() {
   return (
     <>
-      <CancelValidatorModal />
-      <ArtDumpsterAddArtworkPage />
+      <ArtDumpsterHeroSectionPage />
     </>
   );
 }
