9066079a048374cc487658e0152375ddbe27422a
Jian Lee <ramosjianlee@gmail.com>
2025-06-20 13:13:31 +0800

    Remove unused modal components and related code from the Art Dumpster project, including display, settings, upload success, cancel validation, and profile pages to streamline the application and improve maintainability.

--- artdump-gallery-for-creatives/src/App.jsx
+++ artdump-gallery-for-creatives/src/App.jsx
@@ -1,32 +1,10 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
-import CancelValidatorModal from "./components/CancelValidatorModal";
-import UploadImageSuccessModal from "./components/UploadImageSuccessModal";
-import ArtDumpsterAddArtworkPage from "./pages/ArtDumpsterAddArtworkPage";
-import ArtDumpsterHeroSectionPage from "./pages/ArtDumpsterHomePage";
-import ArtDumpsterMyDumpsterPage from "./pages/ArtDumpsterMyDumpsterPage";
-import ArtDumpsterProfilePage from "./pages/ArtDumpsterProfilePage";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<ArtDumpsterHeroSectionPage />}></Route>
-          <Route
-            path="/MyDumpsterPage"
-            element={<ArtDumpsterMyDumpsterPage />}
-          ></Route>
-          <Route
-            path="/AddArtworkPage"
-            element={<ArtDumpsterAddArtworkPage />}
-          ></Route>
-          <Route
-            path="/ProfilePage"
-            element={<ArtDumpsterProfilePage />}
-          ></Route>
-        </Routes>
-      </BrowserRouter>
+      <h1>Hello World!</h1>
+      <h1>ArtDumpster* Redevelopment!</h1>
     </>
   );
 }
