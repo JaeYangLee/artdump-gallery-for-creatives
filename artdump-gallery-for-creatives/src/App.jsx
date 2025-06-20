04c65204f7ff617055849b50ea4ecaf660dbc353
Jian Lee <ramosjianlee@gmail.com>
2025-06-20 14:21:45 +0800

    feat: implement ArtDumpsterHomePage component and update navigation bar styling

--- artdump-gallery-for-creatives/src/App.jsx
+++ artdump-gallery-for-creatives/src/App.jsx
@@ -1,10 +1,12 @@
 import ArtDumpsterNavBar from "./components/ArtDumpsterNavBar";
 import "./index.css";
+import ArtDumpsterHomePage from "./pages/ArtDumpsterHomePage";
 
 function App() {
   return (
     <>
-      <ArtDumpsterNavBar />
+      <ArtDumpsterNavBar className="fixed top-0 z-60" />
+      <ArtDumpsterHomePage />
     </>
   );
 }
---
+++ artdump-gallery-for-creatives/src/pages/ArtDumpsterHomePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function ArtDumpsterHomePage() {
+  return (
+    <>
+      <div className="w-screen min-h-screen bg-[lightgray]">
+        <h1>Hello World!</h1>
+      </div>
+      <div></div>
+    </>
+  );
+}
+
+export default ArtDumpsterHomePage;
