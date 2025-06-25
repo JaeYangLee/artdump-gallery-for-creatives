e64b5c81a77bb9f44d0373d7bde0ef8f36ba39d9
Jian Lee <ramosjianlee@gmail.com>
2025-06-25 17:07:18 +0800

    fix: improve layout and styling of ArtDumpArtworkDisplayModal for better user experience and responsiveness

--- artdump-gallery-for-creatives/src/components/ArtDumpArtworkDisplayModal.jsx
+++ artdump-gallery-for-creatives/src/components/ArtDumpArtworkDisplayModal.jsx
@@ -7,85 +7,78 @@ function ArtDumpArtworkDisplayModal({
   if (!isArtworkDisplayModalOpen) return null;
   return (
     <>
+      {/* Backdrop Container */}
       <div
-        className="fixed top-0 z-50 flex items-center justify-center w-screen min-h-screen p-2 bg-black/80 font-barlow"
+        className="fixed top-0 flex items-center justify-center w-screen min-h-screen p-2 xl:p-8 bg-black/70 z-60 font-barlow"
         onClick={onArtworkDisplayModalClose}
       >
-        <div className="md:p-2 md:pt-0 md:w-screen md:min-h-screen">
-          <div
-            className="mt-8 flex flex-col items-center bg-white max-w-screen h-[593px]"
-            onClick={(e) => e.stopPropagation()}
-          >
-            <header className="flex justify-between items-center w-full p-2 border-b border-[#372F3D]/40">
-              <h1 className="text-base font-medium text-[#372F3D]">
-                Project Title
-              </h1>
-              <div className="flex flex-row gap-2 text-[#372F3D]">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  alt="Settings Icon"
-                  className="size-6"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M4.5 12a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Zm6 0a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Zm6 0a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  alt="X icon"
-                  className="size-6"
-                  onClick={onArtworkDisplayModalClose}
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-              </div>
-            </header>
-            <div className="flex flex-col flex-wrap md:relative md:flex-row">
-              <div className="md:relative flex flex-col justify-center item-center w-full md:w-[70%] lg:w-[70%] lg:h-[554px] md:h-[554px] h-[309px] bg-[#161618]">
+        <div
+          className="xl:pt-0 pt-8 relative flex items-center justify-center w-screen h-[580px]"
+          onClick={(e) => e.stopPropagation()}
+        >
+          {/* Child Container 1 */}
+          <div className="absolute flex flex-col items-center w-full h-full">
+            {/* Child Container 2 */}
+            <div className="p-2 border-b border-black/20 w-full h-14 bg-[#F8FAFC]">
+              <header className="flex flex-row items-center justify-between w-full h-full">
+                <h1 className="text-base font-medium">Project Title</h1>
+                <div className="flex flex-row items-center gap-4">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    alt="Artwork Settings Button"
+                    className="size-6"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M4.5 12a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Zm6 0a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Zm6 0a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    alt="X icon"
+                    className="size-6"
+                    onClick={onArtworkDisplayModalClose}
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </div>
+              </header>
+            </div>
+            {/* Child Container 3 */}
+            <div className="flex flex-col items-center w-full h-full md:flex-row">
+              {/* Child Container 4 */}
+              <div className="w-full bg-[#161618] h-[60%] md:w-[70%] md:h-full">
                 <img
                   src="public/assets/sasuke-retrieval-arc.jpeg"
-                  alt="Artwork Image"
+                  alt="ArtWork Image"
                   className="object-contain w-full h-full"
                 />
               </div>
-              <div className="md:w-[30%] lg:w-[30%] md:h-[554px] lg:h-[554px]">
-                <div className="flex flex-col items-center justify-center p-4 font-barlow font-light text-[#372F3D]/70 italic">
-                  <section className="md:h-[54vh] p-2 pb-0 border rounded-lg border-black/30 placeholder:text-xs w-full h-[50px] text-xs">
-                    Artwork description...
-                  </section>
+              {/* Child Container 5 */}
+              <div className="flex flex-col md:justify-between w-full h-[40%] md:w-[30%] md:h-full bg-[#F8FAFC]">
+                <div className="m-2 w-auto h-[40%] md:min-h-[60%] p-2 border rounded-lg border-[#372F3D]/40">
+                  <p className="text-xs italic light text-[#372F3D]/70">
+                    "Artwork description..."
+                  </p>
                 </div>
-                <div className="flex flex-row items-center justify-start">
-                  <p className="p-2 text-base font-medium">Status</p>
-                  <p>O</p>
-                </div>
-                <hr className="w-full text-black/30" />
-                <div className="flex flex-col items-start justify-center gap-2 p-4">
-                  <div className="flex flex-row items-center justify-center gap-4">
-                    <p className="text-xs font-medium">• Published:</p>
-                    <p className="text-xs italic font-light">00/00/00</p>
-                  </div>
-                  <div className="flex flex-row items-center justify-center gap-4">
-                    <p className="text-xs font-medium">• Medium:</p>
-                    <p className="text-xs italic font-light">Traditional</p>
-                  </div>
-                  <div className="flex flex-row items-center justify-center gap-4">
-                    <p className="text-xs font-medium">• Tools:</p>
-                    <p className="text-xs italic font-light">Ink</p>
-                  </div>
-                  <div className="flex flex-row items-center justify-center gap-4">
-                    <p className="text-xs font-medium">• Genre:</p>
-                    <p className="text-xs italic font-light">Manga</p>
-                  </div>
+                <div>
+                  <h2 className="p-2 text-sm font-medium">Status</h2>
+                  <hr className="w-auto opacity-30"></hr>
+                  <ul className="p-2 text-xs font-medium">
+                    <li>Published:</li>
+                    <li>Medium:</li>
+                    <li>Tools:</li>
+                    <li>Genre:</li>
+                  </ul>
                 </div>
               </div>
             </div>
