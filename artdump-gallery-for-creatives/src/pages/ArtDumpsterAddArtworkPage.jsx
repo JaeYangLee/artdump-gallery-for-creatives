83339cc812d00e42bb4daf7dc8fbc9d570749b7e
Jian Lee <ramosjianlee@gmail.com>
2025-07-02 02:55:30 +0800

    fix: enhance layout and styling of ArtDumpsterAddArtworkPage for better user experience

--- artdump-gallery-for-creatives/src/pages/ArtDumpsterAddArtworkPage.jsx
+++ artdump-gallery-for-creatives/src/pages/ArtDumpsterAddArtworkPage.jsx
@@ -5,11 +5,43 @@ function ArtDumpsterAddArtworkPage() {
   return (
     <>
       <div className="w-screen min-h-screen">
-        <div className="pt-[56px]">
+        <div className="pt-[56px] font-barlow text-[#372F3D]">
+          <header className="p-2">
+            <h1 className="text-3xl font-bold text-shadow-md">
+              Add an artwork!
+            </h1>
+          </header>
           <div className="p-2">
-            <h1>Add an artwork!</h1>
+            {/* Upload Section */}
+            <div className="flex flex-col md:flex-row md:items-start items-center  p-2 max-w-screen min-h-screen bg-[#F8FAFC]">
+              <div className="flex flex-col items-center justify-center w-full bg-gray-200">
+                <div className="flex flex-col items-center justify-center w-full border-2 border-gray-300 border-dashed h-80">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1"
+                    stroke="currentColor"
+                    className="m-2 opacity-60 size-10"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M9 8.25H7.5a2.25 2.25 0 0 0-2.25 2.25v9a2.25 2.25 0 0 0 2.25 2.25h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25H15m0-3-3-3m0 0-3 3m3-3V15"
+                    />
+                  </svg>
+
+                  <h2>Drop files to upload</h2>
+                  <h2>
+                    or{" "}
+                    <a className="text-purple-500 hover:underline hover:cursor-pointer">
+                      browse
+                    </a>
+                  </h2>
+                </div>
+              </div>
+            </div>
           </div>
-          <div></div>
         </div>
         <img
           src="./assets/grid-line-background-image.svg"
