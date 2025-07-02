42a60e2b5980c82553c57931269954ec21a6b623
Jian Lee <ramosjianlee@gmail.com>
2025-07-02 10:42:19 +0800

    feat: improve layout and styling of ArtDumpsterAddArtworkPage with form enhancements and submit/cancel buttons

--- artdump-gallery-for-creatives/src/pages/ArtDumpsterAddArtworkPage.jsx
+++ artdump-gallery-for-creatives/src/pages/ArtDumpsterAddArtworkPage.jsx
@@ -9,11 +9,11 @@ function ArtDumpsterAddArtworkPage() {
           <h1 className="text-3xl font-bold text-shadow-md">Add an artwork!</h1>
         </header>
 
-        <div className="">
-          <div className="flex flex-col md:flex-row md:items-start p-2 min-w-screen h-full bg-[#F8FAFC]">
+        <div className="relative w-screen h-auto p-2 shadow-lg">
+          <div className="flex flex-col md:flex-row md:items-start p-2 w-full h-full bg-[#F8FAFC] shadow-lg">
             {/* Upload Section */}
-            <div className="flex flex-col items-center justify-center w-full bg-gray-200">
-              <div className="flex flex-col items-center justify-center w-full border-2 border-gray-300 border-dashed h-80">
+            <div className="flex flex-col items-center justify-center w-full h-full bg-gray-200 md:w-128 md:h-full">
+              <div className="flex flex-col items-center justify-center w-full h-full border-2 border-gray-300 border-dashed">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -39,7 +39,10 @@ function ArtDumpsterAddArtworkPage() {
               </div>
             </div>
             {/* Project Detail Section */}
-            <form className="flex flex-col justify-start gap-2 p-2 pt-4 text-sm text-[#372F3D]">
+            <form
+              action="/submit"
+              className="flex flex-col justify-start gap-2 p-2 pt-4 text-sm text-[#372F3D] w-full"
+            >
               <div className="flex flex-col">
                 <label htmlFor="projectTitle" className="font-bold">
                   Project Title
@@ -48,7 +51,7 @@ function ArtDumpsterAddArtworkPage() {
                   required
                   type="text"
                   placeholder="Enter project title..."
-                  className="p-2 border border-gray-300 rounded-lg"
+                  className="p-2 bg-gray-200 border border-gray-300 rounded-lg"
                 />
               </div>
               <div className="flex flex-col">
@@ -60,32 +63,56 @@ function ArtDumpsterAddArtworkPage() {
                   type="text"
                   rows="5"
                   placeholder="Enter project description..."
-                  className="p-2 border border-gray-300 rounded-lg"
+                  className="p-2 bg-gray-200 border border-gray-300 rounded-lg"
                 />
               </div>
               <div className="flex flex-col">
                 <label htmlFor="projectTitle" className="font-bold">
                   Medium
                 </label>
-                <select required id="">
-                  <option value="">Medium</option>
+                <select
+                  required
+                  id=""
+                  className="p-2 text-[#372F3D] bg-gray-200 border border-gray-300 rounded-lg"
+                >
+                  <option value="">Select a medium</option>
+                  <option value="">Example 1</option>
                 </select>
               </div>
               <div className="flex flex-col">
                 <label htmlFor="projectTitle" className="font-bold">
                   Tools
                 </label>
-                <select required id="">
-                  <option value="">Medium</option>
+                <select
+                  required
+                  id=""
+                  className="p-2 text-[#372F3D] bg-gray-200 border border-gray-300 rounded-lg"
+                >
+                  <option value="">Select a tool</option>
                 </select>
               </div>
               <div className="flex flex-col">
                 <label htmlFor="projectTitle" className="font-bold">
                   Genre
                 </label>
-                <select required id="">
-                  <option value="">Medium</option>
+                <select
+                  required
+                  id=""
+                  className="p-2 text-[#372F3D] bg-gray-200 border border-gray-300 rounded-lg"
+                >
+                  <option value="">Select a genre</option>
                 </select>
+              </div>
+              <div className="flex flex-row items-end justify-end gap-2">
+                <button
+                  type="submit"
+                  className="p-2 font-bold text-white bg-purple-500 border border-transparent rounded-lg "
+                >
+                  Submit
+                </button>
+                <button className="p-2 border border-gray-300 rounded-lg">
+                  Cancel
+                </button>
               </div>
             </form>
           </div>
