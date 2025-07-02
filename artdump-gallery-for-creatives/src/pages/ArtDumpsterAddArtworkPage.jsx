e788b31b1562784322853768b344f407326ec7e6
Jian Lee <ramosjianlee@gmail.com>
2025-07-02 22:30:58 +0800

    feat: enhance responsiveness and styling of ArtDumpsterAddArtworkPage for better user experience

--- artdump-gallery-for-creatives/src/pages/ArtDumpsterAddArtworkPage.jsx
+++ artdump-gallery-for-creatives/src/pages/ArtDumpsterAddArtworkPage.jsx
@@ -5,14 +5,16 @@ function ArtDumpsterAddArtworkPage() {
   return (
     <>
       <div className="pt-[56px] font-barlow text-[#372F3D]">
-        <header className="p-2">
-          <h1 className="text-3xl font-bold text-shadow-md">Add an artwork!</h1>
+        <header className="p-2 lg:pt-3">
+          <h1 className="text-3xl font-bold text-shadow-md md:text-4xl lg:text-5xl">
+            Add an artwork!
+          </h1>
         </header>
 
-        <div className="relative w-screen h-auto p-2">
-          <div className="flex flex-col md:flex-row md:items-start p-2 w-full h-full bg-[#F8FAFC] shadow-lg">
+        <div className="relative max-w-screen max-h-screen lg:h-[520px] p-2 lg:p-4 xl:p-4 lg:pt-0 xl:pt-0">
+          <div className="flex flex-col md:flex-row md:items-start p-2 lg:p-3 w-full h-auto xl:h-[82vh] bg-[#F8FAFC] shadow-lg shadow-black/30">
             {/* Upload Section */}
-            <div className="flex flex-col items-center justify-center w-full bg-gray-200 h-80 md:w-128 md:h-full">
+            <div className="flex flex-col items-center justify-center w-full bg-gray-200 h-80 md:w-300 md:h-124 lg:h-176 lg:w-380 xl:h-full">
               <div className="flex flex-col items-center justify-center w-full h-full border-2 border-gray-300 border-dashed">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -20,7 +22,7 @@ function ArtDumpsterAddArtworkPage() {
                   viewBox="0 0 24 24"
                   stroke-width="1"
                   stroke="currentColor"
-                  className="m-2 opacity-60 size-10"
+                  className="m-2 opacity-60 size-10 lg:size-20"
                 >
                   <path
                     stroke-linecap="round"
@@ -29,10 +31,10 @@ function ArtDumpsterAddArtworkPage() {
                   />
                 </svg>
 
-                <h2>Drop files to upload</h2>
-                <h2>
-                  or{" "}
-                  <a className="text-purple-500 hover:underline hover:cursor-pointer">
+                <h2 className="lg:text-2xl">Drop files to upload</h2>
+                <h2 className="lg:text-2xl">
+                  or
+                  <a className="pl-1 lg:pl-2 text-purple-500 hover:underline hover:cursor-pointer lg:text-2xl">
                     browse
                   </a>
                 </h2>
@@ -41,10 +43,10 @@ function ArtDumpsterAddArtworkPage() {
             {/* Project Detail Section */}
             <form
               action="/submit"
-              className="flex flex-col justify-start gap-2 p-2 pt-4 text-sm text-[#372F3D] w-full"
+              className="flex flex-col justify-start gap-2 p-2  lg:p-3 pt-4 text-sm text-[#372F3D] w-full"
             >
-              <div className="flex flex-col">
-                <label htmlFor="projectTitle" className="font-bold">
+              <div className="flex flex-col lg:text-lg">
+                <label htmlFor="projectTitle" className="font-bold ">
                   Project Title
                 </label>
                 <input
@@ -52,10 +54,10 @@ function ArtDumpsterAddArtworkPage() {
                   id="projectTitle"
                   type="text"
                   placeholder="Enter project title..."
-                  className="p-2 bg-gray-200 border border-gray-300 rounded-lg focus:border-purple-500 focus:outline invalid:border-red-500 valid:border-green-500"
+                  className="p-2 bg-gray-200 border border-gray-300 rounded-lg focus:border-purple-500 focus:outline valid:border-green-500"
                 />
               </div>
-              <div className="flex flex-col">
+              <div className="flex flex-col lg:text-lg">
                 <label htmlFor="projectDescription" className="font-bold">
                   Description
                 </label>
@@ -65,47 +67,49 @@ function ArtDumpsterAddArtworkPage() {
                   type="text"
                   rows="5"
                   placeholder="Enter project description..."
-                  className="p-2 bg-gray-200 border border-gray-300 rounded-lg focus:border-purple-500 focus:outline invalid:border-red-500 valid:border-green-500"
+                  className="p-2 bg-gray-200 border border-gray-300 rounded-lg focus:border-purple-500 focus:outline  valid:border-green-500"
                 />
               </div>
-              <div className="flex flex-col">
+              <div className="flex flex-col lg:text-lg">
                 <label htmlFor="projectMedium" className="font-bold">
                   Medium
                 </label>
                 <select
                   required
                   id="projectMedium"
-                  className="p-2 text-[#372F3D] bg-gray-200 border border-gray-300 rounded-lg focus:border-purple-500 focus:outline invalid:border-red-500 valid:border-green-500"
+                  className="p-2 text-[#372F3D] bg-gray-200 border border-gray-300 rounded-lg focus:border-purple-500 focus:outline  valid:border-green-500"
                 >
                   <option value="">Select a medium</option>
                   <option value="">Example 1</option>
                 </select>
               </div>
-              <div className="flex flex-col">
+              <div className="flex flex-col lg:text-lg">
                 <label htmlFor="projectTool" className="font-bold">
                   Tools
                 </label>
                 <select
                   required
                   id="projectTool"
-                  className="p-2 text-[#372F3D] bg-gray-200 border border-gray-300 rounded-lg focus:border-purple-500 focus:outline invalid:border-red-500 valid:border-green-500"
+                  className="p-2 text-[#372F3D] bg-gray-200 border border-gray-300 rounded-lg focus:border-purple-500 focus:outline  valid:border-green-500"
                 >
                   <option value="">Select a tool</option>
+                  <option value="">Example 1</option>
                 </select>
               </div>
-              <div className="flex flex-col">
+              <div className="flex flex-col lg:text-lg">
                 <label htmlFor="projectGenre" className="font-bold">
                   Genre
                 </label>
                 <select
                   required
                   id="projectGenre"
-                  className="p-2 text-[#372F3D] bg-gray-200 border border-gray-300 rounded-lg focus:border-purple-500 focus:outline invalid:border-red-500 valid:border-green-500"
+                  className="p-2 text-[#372F3D] bg-gray-200 border border-gray-300 rounded-lg focus:border-purple-500 focus:outline  valid:border-green-500"
                 >
                   <option value="">Select a genre</option>
+                  <option value="">Example 1</option>
                 </select>
               </div>
-              <div className="flex flex-row items-end justify-end gap-2">
+              <div className="flex flex-row items-end justify-end gap-2 lg:text-lg lg:pt-8 xl:pt-4">
                 <button
                   type="submit"
                   className="p-2 font-bold text-white bg-purple-500 border border-transparent rounded-lg "
