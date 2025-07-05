403f96b502c68bc16eae394787ac3d79d0006ce6
Jian Lee <ramosjianlee@gmail.com>
2025-07-06 03:11:40 +0800

    feat: add ArtDumpsterSettingsPage component for user settings

---
+++ artdump-gallery-for-creatives/src/pages/ArtDumpsterSettingsPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ArtDumpsterSettingsPage() {
+  return (
+    <>
+      <h1 className="pt-[56px]">Settings Page</h1>
+    </>
+  );
+}
+
+export default ArtDumpsterSettingsPage;
